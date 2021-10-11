39a52797c1d7963a09f667218657eda1698fe4b1
MateuszOwsiak <mateusz.owsiak@student.pk.edu.pl>
2021-10-11 10:56:14 +0200

    Create basic allocation examples.

---
+++ allocation_basics/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Mateusz on 11.10.2021.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    int a = stoi(_Str:argv[2]);
+    int b = stoi(_Str:argv[3]);
+
+
+
+
+    return 0;
+}
