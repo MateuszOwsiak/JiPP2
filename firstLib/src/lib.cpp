d992eb2283bfe7cec70fa34c2b9afcdfba3cffe1
MateuszOwsiak <mateusz.owsiak@student.pk.edu.pl>
2021-10-18 10:18:05 +0200

    Create basic allocation examples.

---
+++ firstLib/src/lib.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Mateusz on 18.10.2021.
+//
+#include "../include/lib.h"
+#include <iostream>
+using namespace std;
+void itIsWork()
+{
+    cout<<"Yes it work!"<<endl;
+}
---
+++ firstLib/src/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Mateusz on 18.10.2021.
+//
+#include "../include/lib.h"
+using namespace std;
+
+int main()
+{
+    itIsWork();
+    return 0;
+}
